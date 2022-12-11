b7bccdbb62bf6d0f85a88f5b2b8c9e45ec39caa1
Pritam paul <pritam21800128@gmail.com>
2022-12-11 15:47:51 +0600

    ppaul

---
+++ src/components/News/News.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './News.css';
+
+const News = () => {
+    return (
+        <div className='container'>
+            <h5 className='pt-1'>আপনার কৃষিভিত্তিক যাবতীয় সমস্যা আমাদের জানাতে মেসেন্জার বাটনে ক্লিক করুন, হটলাইন নম্বর : ০১৯৭৪৯৫৩৪৮৬</h5>
+             </div>
+
+
+    );
+};
+
+export default News;
